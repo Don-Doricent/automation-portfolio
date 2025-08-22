b6de40da7238a236628b38fc37962d400b7a5431
Donald Doricent <ddoricent97@gmail.com>
2025-08-22 12:51:29 -0400

    added test for invalid login attempt.

--- cypress/e2e/saucedemo/auth.cy.js
+++ cypress/e2e/saucedemo/auth.cy.js
@@ -1,4 +1,4 @@
-describe('Authentication Test', () => {
+describe('Valid Login Attempt', () => {
     it('check credentials', () => {
     
     cy.visit('https://www.saucedemo.com/')
@@ -7,3 +7,12 @@ describe('Authentication Test', () => {
     cy.get('[data-test="login-button"]').click()
     })
 });
+
+describe('Invalid Login Attempt', () => {
+    it('check credentials', () => {
+       cy.visit('https://www.saucedemo.com/') 
+        cy.get('[data-test="username"]').type('standard_user1')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
+    })
+})
